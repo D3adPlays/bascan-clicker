82a7ab4e72fe7055ea4e42b6d347fc14a34d07f5
Cavernos <louisdescavernes@gmail.com>
2021-11-16 22:16:25 +0100

    Add Project in the new branch

--- src/fr/Cavernos/bascanclicker/server/BasicServer.java
+++ src/fr/Cavernos/bascanclicker/server/BasicServer.java
@@ -11,6 +11,8 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class BasicServer {
+	
+
 
 	  public static void main(String[] args) throws Exception {
 		    HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
@@ -46,7 +48,7 @@ public class BasicServer {
 			public void handle(HttpExchange httpExchange) throws IOException {
 				StringBuilder response = new StringBuilder();
 				 Map <String,String>parms = BasicServer.queryToMap(httpExchange.getRequestURI().getQuery());
-				response.append("https://www.google.com/recaptcha/api/siteverify?secret=" + parms.get("6Ld1iTsdAAAAAIVUgfanoRz_nmCCnez_pWKVcz9n" + "response=" + parms.get("03AGdBq25jVcrRziEKsV2hH6Mx3amSiJxFAh-Qc87teIYt9g6suuo6Pj3u77J2FlsG54eIjYgM68BdfA4k-M56flegPztuNSaG8oTB44Fl6YbViXvY72PQR8OJ3CAAt-kTSthi-HAeeeyNLBjQdI49Zi7Y_kkvCPdSYQWj9V1hKLeec06gtSlG3SPoR-zvUiFl9PIchytF5FkY6nI7Q9_Oxc1TFdpPvcIKG8tvYWdiPukwa2Sx5juyALkdiaLwOl85215Jpa0tLCqS8UOVxOKFNcQSMPzzubjHAANpuT8M9eAJpKHY4aamjg7kuLFAVGKIs5xjC9C5_wURbzZQS3rKD3_-VI7VHz-9lFbX8gJXmVsGslTakNV7sSYDvKpjYtmiqGkLruzl9g5iCPZmla3KIHUp1bTIROWUW4O8wSLziB2jg-slxGJp4pFW5H2DVlo-i0MLkSWYksVfxZ0LjRMtQII32oxqvm9r2h7Lb6r_rEUHIFxCfm3dvSqsJc1OLzHSC4KLOUN6YXIa")));
+				 response.append("https://www.google.com/recaptcha/api/siteverify?secret=6Ld1iTsdAAAAAIVUgfanoRz_nmCCnez_pWKVcz9n?response=" + parms.get("key"));
 				BasicServer.writeResponse(httpExchange, response.toString());
 			}
 			  

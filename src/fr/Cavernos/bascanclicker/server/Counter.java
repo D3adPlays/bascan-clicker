05768a683a876c42834fc69e815d7b592d6ec8a0
Cavernos <louisdescavernes@gmail.com>
2021-12-13 21:31:48 +0100

    Counter is now full functionnal

--- src/fr/Cavernos/bascanclicker/server/Counter.java
+++ src/fr/Cavernos/bascanclicker/server/Counter.java
@@ -11,6 +11,7 @@ public class Counter implements HttpHandler{
     Integer count = 0;
     FileWriter counter;
 
+	@SuppressWarnings("unused")
 	public void handle(HttpExchange httpExchange) throws IOException {
 		httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin","*");
 		StringBuilder response = new StringBuilder();
@@ -29,6 +30,46 @@ public class Counter implements HttpHandler{
 				response.append("400");
 			}
 				
+		 } else if (JsonReader.main(parms.get("seconde"))+ BasicServer.encryptKey != null){
+			 String cookie = httpExchange.getRemoteAddress().getAddress().toString();
+				if(BasicServer.decrypt(BasicServer.encryptKey, BasicServer.serverKey).equals(cookie + BasicServer.serverToken)){
+					count++; 
+					response.append("File is Writed");
+					counter = new FileWriter("counterSeconde.txt");
+					counter.write(count.toString());
+					counter.flush();
+					
+				
+				} else{
+					response.append("400");
+				}
+		 } else if(JsonReader.main(parms.get("première"))+ BasicServer.encryptKey != null){
+			 String cookie = httpExchange.getRemoteAddress().getAddress().toString();
+				if(BasicServer.decrypt(BasicServer.encryptKey, BasicServer.serverKey).equals(cookie + BasicServer.serverToken)){
+					count++; 
+					response.append("File is Writed");
+					counter = new FileWriter("counterPremière.txt");
+					counter.write(count.toString());
+					counter.flush();
+					
+				
+				} else{
+					response.append("400");
+				}
+			 
+		 }	else if(JsonReader.main(parms.get("terminale"))+ BasicServer.encryptKey != null){
+			 String cookie = httpExchange.getRemoteAddress().getAddress().toString();
+				if(BasicServer.decrypt(BasicServer.encryptKey, BasicServer.serverKey).equals(cookie + BasicServer.serverToken)){
+					count++; 
+					response.append("File is Writed");
+					counter = new FileWriter("counterTerminale.txt");
+					counter.write(count.toString());
+					counter.flush();
+					
+				
+				} else{
+					response.append("400");
+				}
 		 }
 		BasicServer.writeResponse(httpExchange, response.toString());
 	}
